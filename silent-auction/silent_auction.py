d6f726aeff147392adf7c3b99bc03bfb330d72fa
hilfger04 <149435470+hilfger04@users.noreply.github.com>
2026-08-05 18:11:02 +0200

    Removed unused code

--- silent-auction/silent_auction.py
+++ silent-auction/silent_auction.py
@@ -1,16 +1,9 @@
 import art
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
 print(art.logo)
 
 def find_highest_bidder(bidding_dictionary):
     winner = ""
     highest_bid = 0
-
-    # max_bid = max(dictionary, key=dictionary.get)
-    # print(max_bid)
 
     for bidder in bidding_dictionary:
         bid_amount = bidding_dictionary[bidder]

d035c26e27f58533400f6f2158201ee4be7e35ee
hilfger04 <149435470+hilfger04@users.noreply.github.com>
2026-09-16 23:35:46 +0200

    Add files via upload

--- snake_game/main.py
+++ snake_game/main.py
@@ -27,22 +27,21 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
     if snake.head.distance(food) < 15:
         scoreboard.increase_score()
         food.refresh()
         snake.extend()
 
-    #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
-    #detect collision with tail
     for square in snake.squares[1:]:
+        if square == snake.head:
+            pass
         if snake.head.distance(square) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
